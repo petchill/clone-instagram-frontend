c8379f32a16d661d330796b4a934a3ccbf52055a
petchill <songpon111333@gmail.com>
2025-09-04 20:08:02 +1000

    feat: auth to exchange access token

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useGoogleLogin } from "@react-oauth/google";
+import { API_BASE_URL } from "./const/env";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const postToGetAccessToken = async (authCode: string) => {
+    const apiBaseURL = API_BASE_URL;
+    const response = await fetch(`${apiBaseURL}/auth/accessToken`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: authCode,
+      }),
+    });
+
+    const resJSON = await response.json();
+    return resJSON;
+  };
+
+  const login = useGoogleLogin({
+    flow: "auth-code",
+    scope:
+      "openid email profile https://www.googleapis.com/auth/userinfo.profile",
+    onSuccess: async ({ code }) => {
+      // Exchange the code on your backend for tokens
+      const resp = await postToGetAccessToken(code);
+      console.log(resp);
+    },
+    onError: () => console.log("Login Failed"),
+  });
 
   return (
     <>
+      <button onClick={() => login()}>Sign in with Google</button>;
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +60,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
